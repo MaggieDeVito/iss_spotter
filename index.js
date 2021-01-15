60cbd1ecbced9375f8a8b318ff2964cec3f89eef
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-15 03:35:36 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const { nextISSTimesForMyLocation } = require('./iss')
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+// fetchCoordsByIP('24.86.200.227', (error, coordinates) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned coordinates:' , coordinates);
+// });
+
+// const exampleCoords = { latitude: 49.2958, longitude: -123.141 };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, flyOverTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned flyOverTimes:' , flyOverTimes);
+// });
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
